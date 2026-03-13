0ddcfa85bd034ba4a5ce88efee8fc768db2f00f5
jjwang1118 <jjwang1118@gmail.com>
2026-03-13 16:36:04 +0800

    add "if" in train.py

---
+++ train.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+    print("This is the main entry point for the training script.")
+
+    if exp==0:
+        # baseline
+        pass
+
+    elif exp==1:
+        pass
+    
+    elif exp==2:
+        pass
+
+    elif exp==3:
+        pass
